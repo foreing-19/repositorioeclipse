ab8c0ff4644336fcf032808b64d7f975ea4e6166
usuario <usuario@usuario-PC>
2020-02-17 10:11:53 +0100

    commit 1

--- Pruebas/src/proyecto1.java
+++ Pruebas/src/proyecto1.java
@@ -6,6 +6,7 @@ public class proyecto1 {
 	        for (num=1;num<=20;num=num+2) {
 	            producto=producto*num;
 	            System.out.println("los numeros impares son:"+producto);
+	            System.out.println("los numeros impares son:"+producto);
 	}   
 	    }
 	    
